728988adcd3014ee317f75af280cd76c66ee1456
Yongji Shen <yos036@ucsd.edu>
2021-11-03 01:46:47 +0800

    test

--- deployscript/afterInstall.sh
+++ deployscript/afterInstall.sh
@@ -2,5 +2,8 @@
 touch /home/ubuntu/log.txt
 date >> /home/ubuntu/log.txt
 echo "afterInstall" >> /home/ubuntu/log.txt
-ls -al /home/ubuntu/webapp >> /home/ubuntu/log.txt
+echo "/home/ubuntu/codedeploy/" >> /home/ubuntu/log.txt
+ls -al /home/ubuntu/codedeploy/ >> /home/ubuntu/log.txt
+echo "/home/ubuntu/webapp/" >> /home/ubuntu/log.txt
+ls -al /home/ubuntu/webapp/ >> /home/ubuntu/log.txt
 echo "********************" >> /home/ubuntu/log.txt
