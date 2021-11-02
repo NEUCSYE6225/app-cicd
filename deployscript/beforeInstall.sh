8dea1061121d5c1b320cc76254c254e0d3b39e02
Yongji Shen <yos036@ucsd.edu>
2021-11-03 01:09:39 +0800

    test

--- deployscript/beforeInstall.sh
+++ deployscript/beforeInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 touch /home/ubuntu/log.txt
-echo date >> /home/ubuntu/log.txt
+date >> /home/ubuntu/log.txt
 echo "beforeInstall" >> /home/ubuntu/log.txt
 pwd >> /home/ubuntu/log.txt
 echo "********************" >> /home/ubuntu/log.txt
