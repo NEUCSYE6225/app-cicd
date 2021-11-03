12cef162310609ffd291155e1adbafea1079bf05
Yongji Shen <yos036@ucsd.edu>
2021-11-03 13:15:32 +0800

    test

--- deployscript/applicationStart.sh
+++ deployscript/applicationStart.sh
@@ -7,4 +7,6 @@ echo "/home/ubuntu/codedeploy/" >> /home/ubuntu/log.txt
 ls -al /home/ubuntu/codedeploy/ >> /home/ubuntu/log.txt
 echo "/home/ubuntu/webapp/" >> /home/ubuntu/log.txt
 ls -al /home/ubuntu/webapp/ >> /home/ubuntu/log.txt
+cd /home/ubuntu/webapp/
+pm2 start app.js --name webapp --watch --ignore-watch="node_modules"
 echo "********************" >> /home/ubuntu/log.txt
