0cbe471850405417d7133fdde39d88fbe91d0177
Yongji Shen <yos036@ucsd.edu>
2021-11-03 12:06:14 +0800

    test

--- deployscript/afterInstall.sh
+++ deployscript/afterInstall.sh
@@ -2,14 +2,10 @@
 touch /home/ubuntu/log.txt
 date >> /home/ubuntu/log.txt
 echo "afterInstall" >> /home/ubuntu/log.txt
-echo "/home/ubuntu/codedeploy/" >> /home/ubuntu/log.txt
-ls -al /home/ubuntu/codedeploy/ >> /home/ubuntu/log.txt
-ls -al /home/ubuntu/codedeploy/webapp/ >> /home/ubuntu/log.txt
 echo "/home/ubuntu/webapp/" >> /home/ubuntu/log.txt
 ls -al /home/ubuntu/webapp/ >> /home/ubuntu/log.txt
-echo "cp /home/ubuntu/codedeploy/webapp/*.js /home/ubuntu/webapp/" >> /home/ubuntu/log.txt
 sudo cp /home/ubuntu/codedeploy/webapp/*.js /home/ubuntu/webapp/
-sleep 3
+sleep 1
 echo "/home/ubuntu/webapp/" >> /home/ubuntu/log.txt
 ls -al /home/ubuntu/webapp/ >> /home/ubuntu/log.txt
 echo "********************" >> /home/ubuntu/log.txt
--- deployscript/applicationStop.sh
+++ deployscript/applicationStop.sh
@@ -6,4 +6,5 @@ echo "/home/ubuntu/codedeploy/" >> /home/ubuntu/log.txt
 ls -al /home/ubuntu/codedeploy/ >> /home/ubuntu/log.txt
 echo "/home/ubuntu/webapp/" >> /home/ubuntu/log.txt
 ls -al /home/ubuntu/webapp/ >> /home/ubuntu/log.txt
+killall -9 node
 echo "********************" >> /home/ubuntu/log.txt
