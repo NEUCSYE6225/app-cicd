917c82ace58c428ef21b357186d1e58457cd43a6
Yongji Shen <yos036@ucsd.edu>
2021-11-29 01:28:00 +0800

    test for new db

--- webapp/helper.js
+++ webapp/helper.js
@@ -14,22 +14,3 @@ module.exports = {
     validateEmail,
     validatePassword
 }
-// validateEmail("abcd")
-// validateEmail("abcd@abc.com")
-// console.log(validatePassword('a'))
-// console.log(validatePassword('1'))
-// console.log(validatePassword('1Ac'))
-// console.log(validatePassword('aaaaaaaa'))
-// console.log(validatePassword('12345678'))
-// console.log(validatePassword('abc45678'))
-// console.log(validatePassword('aBc12345'))
-
-
-// db.insertinfo ({first_name:'aaa',last_name:'aaa', username:'abccccc', password:'123'})
-// .then((result)=>{
-//     console.log(result)
-// })
-// .catch((err)=>{
-//     console.log("err:"+err)
-// })
-// db.getinfo({username:'abc'})
